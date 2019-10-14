970397963030f281f9ae640cbda04feb28cd361d
BackTech <backtech@BackTechnoMacBook-ea.local>
2019-10-14 23:04:41 +0900

    コメント削除

--- src/App.js
+++ src/App.js
@@ -14,67 +14,6 @@ class App extends React.Component {
     };
   }
 
-  // componentDidMount() {
-  //   console.log("App didMount start");
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     if (user) {
-  //       console.log('is login');
-  //       this.setState({
-  //         isLoggedIn: true,
-  //         user
-  //       });
-  //       this.getCosts();
-  //     } else {
-  //       console.log('is not login');
-  //       this.setState({ isLoggedIn: false });
-  //     }
-  //   });
-  // };
-
-  // async getCosts() {
-  //   console.log("getCosts called");
-  //   if (!this.state.user) {
-  //     console.error("this.state.user false");
-  //     return;
-  //   }
-  //   const userId = this.state.user.uid;
-  //   const myCostsData = await db.collection('users').doc(userId)
-  //     .collection("costs")
-  //     .orderBy('createdAt', 'desc')
-  //     .get();
-  //   const user = await db.collection('users').doc(userId).get();
-  //   const partnerId = user.data().partnerId;
-  //   const partnerCostsData = await db.collection('users').doc(partnerId)
-  //     .collection("costs")
-  //     .orderBy("createdAt", "desc")
-  //     .get();
-
-  //   const myCosts = myCostsData.docs.map(c => {
-  //     return {
-  //       id: c.id,
-  //       amount: c.data().amount,
-  //       createdAt: moment(c.data().createdAt.toDate()).format("YYYY-MM-DD"),
-  //       category: c.data().category
-  //     }
-  //   });
-  //   const partnerCosts = partnerCostsData.docs.map(c => {
-  //     return {
-  //       id: c.id,
-  //       amount: c.data().amount,
-  //       createdAt: moment(c.data().createdAt.toDate()).format("YYYY-MM-DD"),
-  //       category: c.data().category
-  //     }
-  //   });
-
-  //   const difference = _calcDiffrence(myCosts, partnerCosts);
-
-  //   this.setState({
-  //     myCosts,
-  //     partnerCosts,
-  //     difference
-  //   });
-  // }
-
   render() {
     console.log("App render start");
     return (
@@ -91,19 +30,5 @@ class App extends React.Component {
     );
   }
 }
-
-// function _calcDiffrence(myCosts, partnerCosts) {
-//   let myTotalCost = 0;
-//   if (myCosts.length > 0) {
-//     myTotalCost = myCosts.reduce((acc, current) => acc + current.amount, 0);
-//   }
-
-//   let partnerTotalCost = 0;
-//   if (partnerCosts.length > 0) {
-//     partnerTotalCost = partnerCosts.reduce((acc, current) => acc + current.amount, 0);
-//   }
-
-//   return myTotalCost - partnerTotalCost;
-// }
 
 export default App;
