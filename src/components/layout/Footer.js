fe751bcd47201cbe941d630e165ab6e6f0f59d81
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-11-12 10:07:03 +0900

    materializeをmuiに置き換え途中

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,33 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom' ;
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const useStyles = makeStyles({
+  root: {
+    position: "fixed",
+    bottom: 0,
+    width: "100%",
+  },
+});
 
 const Footer = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
   return(
-    <footer className="red lighten-2">
-      <div className="container">
-        <div className="row">
-          <div className="col s4 footer-item">
-            <Link to='/'>
-              <i className="small material-icons">assessment</i>
-              <div><span>集計</span></div>
-            </Link>
-          </div>
-          <div className="col s4 footer-item">
-            <Link to='/new'>
-              <i className="small material-icons">edit</i>
-              <div><span>記録</span></div>
-            </Link>
-          </div>
-          <div className="col s4 footer-item">
-            <Link to='/settings'>
-              <i className="small material-icons">settings</i>
-              <div><span>設定</span></div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction component={Link} to="/" label="集計" icon={<RestoreIcon />} />
+      <BottomNavigationAction component={Link} to="/new"label="記録" icon={<FavoriteIcon />} />
+      <BottomNavigationAction component={Link} to="/settings" label="設定" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
 }
 
 export default Footer
